5cced703a57aaef88177f4c3043397f443d22fea
Vladislav Arbatov <vladzima@Vladislavs-MacBook-Pro.local>
2016-05-29 01:16:28 +0300

    Changes to Readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# nightnight_ru
+# Russian translation for night night.
+
+[nightnight.みんな](http://nightnight.xn--q9jyb4c/) (みんな is Japanese for "everyone") is a simple program that allows you to put your website to sleep, encouraging your users to go to head to bed, better supporting their health and well being.
+
+Kudos to [Masamichi Souzou](http://mmsz.co/).
+
+More information on [Github](https://github.com/nww/nightnight).
+
+## Translation.
+
+All the text visible to visitors translated to Russian and so are console log errors.
--- nightnight_ru.js
+++ nightnight_ru.js
@@ -1,5 +1,5 @@
 /** nightnight.みんな v1.0 | MIT License | https://git.io/vwz9N */
-/** Translated by arb.digital 2016 */
+/** Translation by arb.digital | https://git.io/vrHYt */
 
 window.nightnightHide = function() {
 	var banners = document.getElementsByClassName('nightnight');
@@ -21,12 +21,12 @@ window.nightnightHide = function() {
 	var e = eh * 60 + em;
 
 	if (b < e) {
-		console.log("WARNING: NightNight: invalid time interval: sleep time is earlier in the day than wakeup time");
+		console.log("ВНИМАНИЕ: NightNight: неверный интервал времени: указанное время сна раньше времени пробуждения");
 		return
 	}
 
 	if (m >= b || m <= e) {
-		console.log('NightNight activated')
+		console.log('NightNight запущен')
 
 		// Inject CSS
 		var css = '.nightnight {background: #000; font-family: Arial, sans-serif; color: #fff; font-size: 12px; padding: 10%; position: fixed; margin: 0; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; } .nightnight-skip a {padding: 10px 0; } .nightnight a:link {color: #555; } .nightnight a:visited {color: #555; } .nightnight a:hover {color: #FFF; } .nightnight-timing {font-size: 15px; font-weight: 900; margin-top: 20px; } .nightnight-skip {font-style: italic; font-size: 14px; margin-top: 40px; } .nightnight-credits {position: fixed; bottom: 20px; right: 20px; margin-top: 40px; font-size:9px; }';
@@ -51,9 +51,9 @@ window.nightnightHide = function() {
 
 		// Inject DIV
 		var wrapper = document.createElement('div');
-		wrapper.innerHTML = '<div class="nightnight"><div>It&rsquo;s late&hellip;</div><div> Nothing we can offer you is more important than your sleep.</div> <div> Sleep well, sweet dreams, and we will catch you in the morning.</div> <div class="nightnight-timing">This site will wake up at ' + 
+		wrapper.innerHTML = '<div class="nightnight"><div>Уже поздно&hellip;</div><div> Ничего из того, что мы может вам предложить, важнее вашего сна.</div> <div> Доброй ночи, сладких снов, и мы увидимся с вами утром.</div> <div class="nightnight-timing">Этот сайт проснется в ' + 
 			time + 
-			'</div> <div class="nightnight-skip"> <a href="javascript:nightnightHide()">I&rsquo;ll sleep later, I really need to use this site right now &rightarrow;</a> </div> <div class="nightnight-credits"> <a href="https://www.nightnight.xn--q9jyb4c"><img src="https://code.nightnight.xn--q9jyb4c/static/night_night_everyone_white.png" alt="night night" height="32" width="161"></a></div> </div>';
+			'</div> <div class="nightnight-skip"> <a href="javascript:nightnightHide()">Я посплю позже, мне правда нужно попасть на сайт сейчас &rightarrow;</a> </div> <div class="nightnight-credits"> <a href="https://www.nightnight.xn--q9jyb4c"><img src="https://code.nightnight.xn--q9jyb4c/static/night_night_everyone_white.png" alt="night night" height="32" width="161"></a></div> </div>';
 		document.body.appendChild(wrapper);
 	}
 })();
